540d9b2e28af2868efb819e59bac48a891bc4241
myacobovsky <myacobovsky@gmail.com>
2026-03-10 09:07:47 -0400

    load all fonts for editor Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Montserrat:wght@400;700&family=Pacifico&family=Sour+Gummy:wght@400;700&family=DynaPuff:wght@400;700&family=Delius&family=Emilys+Candy&family=Unica+One&family=Ultra&family=Quantico:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body>
         <Providers>{children}</Providers>
